78021b063398969cb7389e22370857a9ddfa18cf
Vihaan-Taparia <mayataparia@gmail.com>
2025-04-18 21:20:35 +0530

    Add files via upload

---
+++ operators3.py
@@ -0,0 +1,50 @@
+#identity operator
+
+#is, not is
+
+
+a=[20,30]
+b=[20,30]
+c=a
+
+print(a is b)
+print(a is c)
+print(a is not c)
+
+
+#membership operator
+#in, not in
+
+p="HELLO"
+q="HELLO WORLD"
+
+print(p in q)
+print(p not in q)
+
+
+#Bitwise operators
+a=5
+b=3
+
+print(a<<b)
+print(a>>b)
+print(a&b)
+print(a|b)
+print(a^b)
+print(~b)
+
+
+#bitwise left shift
+a=5
+b=3
+print("a<<1=",a<<1)
+print("b<<1=",b<<1)
+
+
+#bitwise right shift
+a=5
+b=3
+print("a>>1=",a>>1)
+print("b>>1=",b>>1)
+
+
